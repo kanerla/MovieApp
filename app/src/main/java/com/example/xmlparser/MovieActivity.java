e48badc561196481c9a9937a818b4bf055c9e192
kanerla <kanervalaurajohanna@gmail.com>
2020-03-14 14:42:22 +0200

    Get two different entries from API. Switch fragments flawlessly.

--- app/src/main/java/com/example/xmlparser/MovieActivity.java
+++ app/src/main/java/com/example/xmlparser/MovieActivity.java
@@ -24,11 +24,14 @@ import androidx.fragment.app.FragmentTransaction;
 
 public class MovieActivity extends AppCompatActivity {
     private static final String URL = "https://www.finnkino.fi/xml/Events/";
+    private static final String ComingURL = "https://www.finnkino.fi/xml/Events/?listType=ComingSoon";
     List<Event> entries;
+    List<Event> coming;
     FragmentManager manager;
     FragmentTransaction transaction;
     NowInTheatresFragment nit_fragment;
     Bundle bundle;
+    Bundle comingbundle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,11 +49,17 @@ public class MovieActivity extends AppCompatActivity {
         transaction = manager.beginTransaction();
 
         loadPage();
+        loadComingPage();
     }
 
     // Uses AsyncTask to download the XML feed from the URL.
     public void loadPage() {
         new DownloadXmlTask().execute(URL);
+    }
+
+    // Uses AsyncTask to download the XML feed from the URL.
+    public void loadComingPage() {
+        new DownloadComingXmlTask().execute(ComingURL);
     }
 
     public void showInfoDialog(Event e) {
@@ -71,15 +80,9 @@ public class MovieActivity extends AppCompatActivity {
         movieinfo.show(fragmentManager, "MovieInfo");
     }
 
-    public void switchFragments(View v) {
+    public void switchToNowInTheatres(View v) {
         manager = getSupportFragmentManager();
-        Fragment fragment = manager.findFragmentById(R.id.fragment_container);
-        if (fragment instanceof NowInTheatresFragment) {
-            fragment = new ComingSoonFragment();
-        } else {
-            fragment = new NowInTheatresFragment();
-        }
-
+        Fragment fragment = new NowInTheatresFragment();
         fragment.setArguments(bundle);
 
         /* CREATES ONE COPY OF FRAGMENT TO BACKSTACK --> back button opens previous fragment
@@ -97,6 +100,17 @@ public class MovieActivity extends AppCompatActivity {
 
         transaction.replace(R.id.fragment_container, fragment);
         // transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+    public void switchToComingSoon(View v) {
+        manager = getSupportFragmentManager();
+        Fragment fragment = new ComingSoonFragment();
+        fragment.setArguments(comingbundle);
+
+        transaction = manager.beginTransaction();
+
+        transaction.replace(R.id.fragment_container, fragment);
         transaction.commit();
     }
 
@@ -195,6 +209,79 @@ public class MovieActivity extends AppCompatActivity {
                 stream = downloadUrl(urlString);
                 entries = xmlParser.parse(stream);
                 Log.d("Entries 1: ", "" + entries.size());
+                // Makes sure that the InputStream is closed after the app is
+                // finished using it.
+                Log.d("MovieActivity", "Streamed and parsed");
+            } finally {
+                if (stream != null) {
+                    stream.close();
+                    Log.d("MovieActivity", "Closed the stream");
+                }
+            }
+
+            // XmlParser returns a List (called "events") of Event objects.
+            // Each Event object represents a single movie in the XML feed.
+            // This section processes the events list to combine each event with HTML markup.
+            // Each event is displayed in the UI as a link that optionally includes
+            // a text summary.
+
+            return htmlString.toString();
+        }
+
+        // Given a string representation of a URL, sets up a connection and gets
+        // an input stream.
+        private InputStream downloadUrl(String urlString) throws IOException {
+            java.net.URL url = new URL(urlString);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(10000 /* milliseconds */);
+            conn.setConnectTimeout(15000 /* milliseconds */);
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+            Log.d("MovieActivity", "all good in the hood");
+            // Starts the query
+            conn.connect();
+            Log.d("MovieActivity", "conn.connect says: No adapter attached; skipping layout");
+            return conn.getInputStream();
+        }
+    }
+
+    private class DownloadComingXmlTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+            try {
+                return loadXmlFromNetwork(urls[0]);
+            } catch (IOException e) {
+                return "Connection error";
+            } catch (XmlPullParserException e) {
+                return "Error parsing";
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            // listassa on tavaraa
+            Log.d("Coming Soon: ", "" + coming.size());
+
+            ArrayList<Event> array = new ArrayList<>(coming);
+            comingbundle = new Bundle();
+            comingbundle.putParcelableArrayList("comingArray", array);
+        }
+
+        // Uploads XML from the URL and parses it. Returns string.
+        private String loadXmlFromNetwork(String urlString) throws XmlPullParserException, IOException {
+            InputStream stream = null;
+            // Instantiate the parser
+            XmlParser xmlParser = new XmlParser();
+            coming = null;
+
+            // Checks whether the user set the preference to include summary text
+            // SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+
+            StringBuilder htmlString = new StringBuilder();
+
+            try {
+                stream = downloadUrl(urlString);
+                coming = xmlParser.parse(stream);
                 // Makes sure that the InputStream is closed after the app is
                 // finished using it.
                 Log.d("MovieActivity", "Streamed and parsed");
